1de87f3fb333c2d88ed6a993661501499682c704
liad <liadd109@gmail.com>
2021-10-26 16:13:48 +0300

    Again

--- tWpfMashUp v0.0.1/tWpfMashUp v0.0.1/obj/Debug/net5.0-windows/App.g.i.cs
+++ tWpfMashUp v0.0.1/tWpfMashUp v0.0.1/obj/Debug/net5.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace tWpfMashUp_v0._0._1 {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/tWpfMashUp v0.0.1;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/tWpfMashUp v0.0.1;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
