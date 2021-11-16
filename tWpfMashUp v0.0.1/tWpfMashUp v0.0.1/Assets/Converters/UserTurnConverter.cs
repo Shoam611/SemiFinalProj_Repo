12167223cf4a667b6f4f936e09ce8fb1240e09f1
liad <liadd109@gmail.com>
2021-11-16 15:50:19 +0200

    New Model

---
+++ tWpfMashUp v0.0.1/tWpfMashUp v0.0.1/Assets/Converters/UserTurnConverter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Globalization;
+using System.Windows;
+using tWpfMashUp_v0._0._1.MVVM.Models;
+using tWpfMashUp_v0._0._1.Sevices;
+
+namespace tWpfMashUp_v0._0._1.Assets.Converters
+{
+    public class UserTurnConverter : BaseConverter<UserTurnConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var store = App.ServiceProvider.GetRequiredService<StoreService>();
+            var user = (User)store.Get(CommonKeys.WithUser.ToString()).UserName;
+
+            if ((bool)value)
+                return "Your turn";
+
+            return $"{user.UserName}";
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
