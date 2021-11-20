ad7d95e9de1eee3b62b165c039d6b212abbfc35a
shoam <shoam611@gmail.com>
2021-11-20 21:29:49 +0200

    Update ChatAppViewModel.cs

--- tWpfMashUp v0.0.1/tWpfMashUp v0.0.1/MVVM/ViewModels/ChatAppViewModel.cs
+++ tWpfMashUp v0.0.1/tWpfMashUp v0.0.1/MVVM/ViewModels/ChatAppViewModel.cs
@@ -179,7 +179,6 @@ namespace tWpfMashUp_v0._0._1.MVVM.ViewModels
                         SelectedUser = user;
                     }
                 }
-                storeService.InformContactChanged(selectionChangedEventArgs.Source,new ChatRecivedEventArgs {});
             }
             catch { }
         }
